f0ef469898dd55dda633965ea6d05e93aaa7f86d
WmBreck <breck@seinigerlaw.com>
2025-07-14 20:21:00 -0400

    Add generateStaticParams to state page to fix static export

--- src/app/state/[state]/page.tsx
+++ src/app/state/[state]/page.tsx
@@ -15,6 +15,21 @@ interface StatePageProps {
   params: Promise<{ state: string }>;
 }
 
+// Generate static params for all states
+export async function generateStaticParams() {
+  const states = [
+    'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
+    'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
+    'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
+    'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
+    'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY', 'DC'
+  ];
+  
+  return states.map((state) => ({
+    state: state,
+  }));
+}
+
 export default async function StatePage({ params }: StatePageProps) {
   const { state: abbr } = await params;
   const fullName = getStateFullName(abbr);
